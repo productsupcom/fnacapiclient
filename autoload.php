7add0a4385957d6a1c506819fe1e4a56b9d33fcc
Nicolai Storz <nicolai.storz@productsup.com>
2019-08-07 14:32:50 +0200

    Added files to autoloader

--- autoload.php
+++ autoload.php
@@ -1,6 +1,4 @@
 <?php
-require_once __DIR__ . '/vendor/autoload.php';
-require_once __DIR__.'/vendor/symfony/class-loader/UniversalClassLoader.php';
 
 use Symfony\Component\ClassLoader\UniversalClassLoader;
 
